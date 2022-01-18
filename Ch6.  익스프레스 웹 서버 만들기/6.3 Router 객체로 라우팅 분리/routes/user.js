760232b37cbbbb3348b5c5178d873854e9f4e7f5
hy2850 <hy2850@gmail.com>
2022-01-18 11:05:23 +0900

    ✏️ Update: Ch6-3 Route parameters and query string
    * Added usage of route parameters and query string

--- Ch6.  익스프레스 웹 서버 만들기/6.3 Router 객체로 라우팅 분리/routes/user.js
+++ Ch6.  익스프레스 웹 서버 만들기/6.3 Router 객체로 라우팅 분리/routes/user.js
@@ -4,7 +4,18 @@ const router = express.Router();
 
 // GET /user 라우터 → 🚨 app.js에서 /user endpoint에서 router 호출하므로, 여기선 '/'에 GET
 router.get('/', (req, res) => {
+  console.log(req.query); // 👉🏻 http://localhost:3000/user?limit=5&skip=10
   res.send('Hello, User');
+});
+
+// 👉🏻 http://localhost:3000/user/1
+router.get('/:id', (req, res) => {
+  res.send(`Hello, User ${req.params.id}`);
+});
+
+// 👉🏻 http://localhost:3000/user/1/A
+router.get('/:id/:class', (req, res) => {
+  res.send(`Hello, User ${req.params.id} at ${req.params.class}`);
 });
 
 export default router;
