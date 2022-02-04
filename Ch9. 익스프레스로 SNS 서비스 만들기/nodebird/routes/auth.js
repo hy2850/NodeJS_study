00899f2f377b4d543a66aaae06ac6a587a1e683f
hy2850 <hy2850@gmail.com>
2022-02-04 17:37:37 +0900

    ✏️ Update: Ch9-3 Passport local strategy
    * Constructed local strategy (sign-up, sign-in, sign-out) for Passport

---
+++ Ch9. 익스프레스로 SNS 서비스 만들기/nodebird/routes/auth.js
@@ -0,0 +1,60 @@
+// 회원가입, 로그인, 로그아웃 라우터
+const express = require('express');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const User = require('../models/user');
+
+const router = express.Router();
+
+// 회원가입
+router.post('/join', isNotLoggedIn, async (req, res, next) => {
+  const { email, nick, password } = req.body;
+  try {
+    const exUser = await User.findOne({ where: { email } });
+    if (exUser) {
+      return res.redirect('/join?error=exist');
+    }
+    const hash = await bcrypt.hash(password, 12); // crypto 모듈의 pbkdf2로 대체 가능
+    await User.create({
+      email,
+      nick,
+      password: hash,
+    });
+    return res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
+
+// 로그인
+router.post('/login', isNotLoggedIn, (req, res, next) => {
+  // 👉🏻 Strategy 수행 (local strategy 구현은 passport/localStrategy.js에서)
+  passport.authenticate('local', (authError, user, info) => {
+    if (authError) {
+      console.error(authError);
+      return next(authError);
+    }
+    if (!user) {
+      return res.redirect(`/?loginError=${info.message}`);
+    }
+
+    // ⭐️ req.login이 serializeUser 호출 + user 객체 넘겨줌
+    return req.login(user, (loginError) => {
+      if (loginError) {
+        console.error(loginError);
+        return next(loginError);
+      }
+      return res.redirect('/');
+    });
+  })(req, res, next); // ✨ 미들웨어 내의 미들웨어에는 (req, res, next)를 붙입니다.
+});
+
+router.get('/logout', isLoggedIn, (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.redirect('/'); // 메인 페이지로 돌아가기
+});
+
+module.exports = router;
