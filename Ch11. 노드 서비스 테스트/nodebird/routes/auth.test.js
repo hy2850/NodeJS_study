5593b866a21fff5befb09178eb2e4c42942f1ad4
hy2850 <hy2850@gmail.com>
2022-02-14 15:25:10 +0900

    ✏️ Update: Ch11-4 join before login - user exists error on join
    * Test join (sign-up) first
    * Test login when already logged-in (supertest `agent`)
    * Test fails when ran again - signed-up user data exists from previous test, so sign-up test fails

--- Ch11. 노드 서비스 테스트/nodebird/routes/auth.test.js
+++ Ch11. 노드 서비스 테스트/nodebird/routes/auth.test.js
@@ -6,15 +6,46 @@ beforeAll(async () => {
   await sequelize.sync(); // 데이터베이스 테이블 생성
 });
 
-describe('POST /login', () => {
-  test('로그인 수행', (done) => {
+describe('POST /join', () => {
+  // 회원가입 테스트
+  test('로그인 안 했으면 가입', (done) => {
     request(app)
+      .post('/auth/join')
+      .send({
+        email: 'zerohch0@gmail.com', // req.body에 들어감
+        nick: 'zerocho',
+        password: 'nodejsbook',
+      })
+      .expect('Location', '/')
+      .expect(302, done); // redirect라서 302인 듯
+  });
+});
+
+describe('POST /login', () => {
+  const agent = request.agent(app); // 로그인 해놓을 계정이라고 생각
+
+  // 각 테스트에 앞서 먼저 실행되는 부분 - 로그인 수행
+  beforeEach((done) => {
+    agent
       .post('/auth/login')
       .send({
         email: 'zerohch0@gmail.com',
         password: 'nodejsbook',
       })
-      .expect('Location', '/') // ❌ Test fails here - 가입되지 않는 회원이라
+      .end(done);
+  });
+
+  // POST auth/login 의 isNotLoggedIn 테스트
+  test('이미 로그인했으면 redirect /', (done) => {
+    const message = encodeURIComponent('로그인한 상태입니다.');
+    agent
+      .post('/auth/join')
+      .send({
+        email: 'zerohch0@gmail.com',
+        nick: 'zerocho',
+        password: 'nodejsbook',
+      })
+      .expect('Location', `/?error=${message}`)
       .expect(302, done);
   });
 });
